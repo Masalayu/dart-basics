94f4089f4d010a1eb0de0012f62a3e84f008738b
ibad_pwl_uas <nikmahibad@gmail.com>
2026-03-13 21:59:27 +0700

    bunga

---
+++ dart_project/bin/hitung_bunga.dart
@@ -0,0 +1,8 @@
+void main() {
+  double p = 1000; // Principal (Pinjaman awal)
+  double t = 2;    // Time (Waktu dalam tahun)
+  double r = 5;    // Rate (Bunga tahunan dalam %)
+
+  double bunga = (p * t * r) / 100;
+  print("Bunga Sederhana: $bunga");
+}
