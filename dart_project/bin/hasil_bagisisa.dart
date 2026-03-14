8eda88930abebb27635b0fbd8c44baeb21751e7a
ibad_pwl_uas <nikmahibad@gmail.com>
2026-03-14 22:34:55 +0700

    hasilbagisisa

---
+++ dart_project/bin/hasil_bagisisa.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  int hasilBagi = a ~/ b; // Operator ~/ untuk pembagian bulat
+  int sisaBagi = a % b;
+
+  print("Hasil bagi: $hasilBagi");
+  print("Sisa bagi: $sisaBagi");
+}
