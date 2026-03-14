98ae00f11df1a79f705908f9855d32f0a9e4740a
ibad_pwl_uas <nikmahibad@gmail.com>
2026-03-14 15:01:19 +0700

    print_nama

---
+++ dart_project/bin/print_nama.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Nama Depan: ");
+  String? depan = stdin.readLineSync();
+  stdout.write("Nama Belakang: ");
+  String? belakang = stdin.readLineSync();
+
+  print("Nama Lengkap: $depan $belakang");
+}
